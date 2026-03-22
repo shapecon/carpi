18bd359508f1a73c54f19581ad15ea8a05db90a0
cpfeilschifter <cpfeilschifter@gmail.com>
2026-03-22 18:59:13 +0100

    change url repo

--- src/renderer/src/components/layouts/Layout.tsx
+++ src/renderer/src/components/layouts/Layout.tsx
@@ -2,7 +2,13 @@ import { Outlet } from 'react-router'
 
 export const Layout = () => {
   return (
-    <div id="main-root">
+    <div
+      id="main-root"
+      style={{
+        width: '100%',
+        height: '100%'
+      }}
+    >
       <Outlet />
     </div>
   )
